397948eeaabb2ab3eff802940ea0d577795f8728
jclizar <jessica_lizar@yahoo.com>
2024-09-06 19:20:38 -0300

    pagination location and episode

--- routes.py
+++ routes.py
@@ -5,6 +5,28 @@ app = Flask(__name__)
 
 URLBASE = "https://rickandmortyapi.com/api"
 
+def pagination_att(page,data,endpoint):
+    pagination = {}
+    pagination["page"] = page
+
+    pagination["first"] = f"{endpoint}?page={1}"
+    if page == 1:
+        pagination["prev"] = f"{endpoint}?page={1}"
+        pagination["prev_pag"] = 1
+    else:
+        pagination["prev"] = f"{endpoint}?page={page-1}"
+        pagination["prev_pag"] = page-1
+
+    pagination["last"] = f"{endpoint}?page={data['info']['pages']}"
+    pagination["last_num"] = data['info']['pages']
+    if page == data['info']['pages']: #max == 42
+        pagination["next"] = f"{endpoint}?page={data['info']['pages']}"
+        pagination["next_pag"] = data['info']['pages']
+    else:
+        pagination["next"] = f"{endpoint}?page={page+1}"
+        pagination["next_pag"] = page+1
+
+    return pagination
 # Rota principal que exibe a lista de personagens
 @app.route("/")
 def get_list_characters_page():
@@ -14,36 +36,23 @@ def get_list_characters_page():
     data_one = response.read()
     data = json.loads(data_one)
 
-    pagination = {}
-    pagination["page"] = page
-
-    pagination["first"] = f"/?page={1}"
-    if page == 1:
-        pagination["prev"] = f"/?page={1}"
-        pagination["prev_pag"] = 1
-    else:
-        pagination["prev"] = f"/?page={page-1}"
-        pagination["prev_pag"] = page-1
-
-    pagination["last"] = f"/?page={data['info']['pages']}"
-    if page == data['info']['pages']: #max == 42
-        pagination["next"] = f"/?page={data['info']['pages']}"
-        pagination["next_pag"] = data['info']['pages']
-    else:
-        pagination["next"] = f"/?page={page+1}"
-        pagination["next_pag"] = page+1
-
+    pagination = pagination_att(page, data, '/')
+    
     return render_template("characters.html", characters=data["results"], pagination=pagination)
 
 # Rota que exibe a lista de episódios
 @app.route("/episodio/")
 def get_episode():
-    url = f"{URLBASE}/episode"
+    page = int(request.args.get('page', 1)) # /?page=1
+    url = f"{URLBASE}/episode?page={page}"
     response = urllib.request.urlopen(url)
     data_two = response.read()
     data = json.loads(data_two)
     episodios = data.get('results', [])
-    return render_template("episode.html", episodios=episodios)
+
+    pagination = pagination_att(page, data, '/episodio')
+
+    return render_template("episode.html", episodios=episodios ,pagination=pagination)
 
 @app.route("/profile/<id>") 
 def get_profile(id):
@@ -59,11 +68,15 @@ def get_profile(id):
 
 @app.route("/locations/")
 def get_locations():
-    url = f"{URLBASE}/location"
+    page = int(request.args.get('page', 1)) # /?page=1
+    url = f"{URLBASE}/location?page={page}"
+    #url = f"{URLBASE}/location"
     response = urllib.request.urlopen(url)
     data = response.read()
     dict = json.loads(data)
-    return render_template("locations.html",locations=dict["results"])
+
+    pagination = pagination_att(page, dict, '/locations')
+    return render_template("locations.html",locations=dict["results"], pagination=pagination)
 
 @app.route("/locations/<id>")
 def get_locations_id(id):
@@ -73,7 +86,7 @@ def get_locations_id(id):
     dict = json.loads(data)
     return render_template("location.html",location=dict)
 
-# Nova rota que exibe o perfil de um episódio específico
+'''# Nova rota que exibe o perfil de um episódio específico
 @app.route("/episode/<id>")
 def episode_profile(id):
     # Faz a requisição para o episódio específico usando o ID
@@ -89,7 +102,7 @@ def episode_profile(id):
         "episode.html", 
         episode=episode, 
         characters=character_urls
-    )
+    )'''
 
 # Rota que retorna uma lista de personagens simplificada em JSON
 @app.route("/lista")
